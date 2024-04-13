946f33cece36fc9d55c7db9dd7bdf324f5216926
SeshiriaC <herynirinacecilia@gmail.com>
2024-04-13 19:30:49 +0300

    UPDATE: Ticket type, ticket type details implementation

--- src/component/TicketType.jsx
+++ src/component/TicketType.jsx
@@ -1,22 +1,27 @@
 import { Divider } from "primereact/divider";
-import { Button } from 'primereact/button';
-
 
 const TicketType = (props) => {
-    return (
-            <div className="mx-2 px-4 pb-4 border-1 surface-border surface-card border-round hover:shadow-8">
-                <div className="flex flex-column align-items-center py-5">
-                    <div className="text-xl text-surface-50 font-bold">{props.category}</div>
-                    <Divider/>
-                    <img className="w-3 border-round" src={props.image} alt={props.category} />
-                    {props.details}
-                </div>
-                <div className="flex align-items-center justify-content-between">
-                    <span className="text-xl text-surface-50 font-semibold">{props.price} MGA</span>
-                    <Button icon="pi pi-shopping-cart" rounded text raised severity="info" aria-label="Buy" className="p-button-rounded"/>
-                </div>
-            </div>
-    );
+  return (
+    <div className="mx-2 px-4 pb-4 border-1 surface-border surface-card border-round hover:shadow-8">
+      <div className="flex flex-column align-items-center py-5">
+        <div className="text-xl text-surface-50 font-bold">
+          {props.category}
+        </div>
+        <Divider />
+        <img
+          className="w-3 border-round"
+          src={props.image}
+          alt={props.category}
+        />
+        <div className="align-items-left">{props.details}</div>
+      </div>
+      <div className="flex align-items-center justify-content-between">
+        <span className="text-xl text-surface-50 font-semibold">
+          {props.price} MGA
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default TicketType;
--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -6,55 +6,63 @@ import premiumImage from "../images/diamond.png";
 import accessImage from "../images/onyx.png";
 
 function Booking() {
-  const accessTicketDetailsList = [{ id: 0, detail: "Bon d'achat" }];
-  const accessTicketDetails = accessTicketDetailsList.filter((detail) => (
+  const accessTicketDetailsList = ["Bon d'achat"];
+  const accessTicketDetails = accessTicketDetailsList.map((detail) => (
     <li>{detail}</li>
   ));
-  const premiumTicketDetails = (
-    <ul>
-      <li>Goodies et bons d'achat</li>
-      <li>Accès au Lounge Bar</li>
-      <li>Meeting privé avec les experts</li>
-      <li></li>
-    </ul>
-  );
 
-  const vipTicketDetails = (
-    <ul>
-      <li>Goodies et bons d'achat</li>
-      <li>Accès au Lounge Bar</li>
-      <li>Meeting privé avec les experts</li>
-      <li>Réductions sur les stands d'achats</li>
-    </ul>
-  );
+  const premiumTicketDetailsList = [
+    "Goodies et bons d'achat",
+    "Accès au Lounge Bar",
+    "Meeting privé avec les experts",
+  ];
+  const premiumTicketDetails = premiumTicketDetailsList.map((detail) => (
+    <li>{detail}</li>
+  ));
+
+  const vipTicketDetailsList = [
+    "Goodies et bons d'achat",
+    "Accès au Lounge Bar",
+    "Meeting privé avec les experts",
+    "Réductions sur les stands d'achats",
+  ];
+
+  const vipTicketDetails = vipTicketDetailsList.map((detail) => (
+    <li>{detail}</li>
+  ));
 
   return (
     <div>
       <Menu />
-      <div className="grid m-4 md:flex md:flex-wrap md:justify-content-center md:align-self-center">
-        <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
-          <TicketType
-            image={accessImage}
-            category="Access"
-            details={accessTicketDetails}
-            price="10.000"
-          />
+      <div className="grid mt-4 justify-content-center align-self-center">
+        <div className="text-blue-900">
+          <h2> Types de ticket </h2>
         </div>
-        <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
-          <TicketType
-            image={premiumImage}
-            category="Premium"
-            details={premiumTicketDetails}
-            price="30.000"
-          />
-        </div>
-        <div className="col-12  sm:col-6 lg:col-12 xl:col-4 p-2">
-          <TicketType
-            image={vipImage}
-            category="VIP"
-            details={vipTicketDetails}
-            price="60.000"
-          />
+        <div className="grid m-4 md:flex md:flex-wrap md:justify-content-center md:align-self-center">
+          <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
+            <TicketType
+              image={accessImage}
+              category="Access"
+              details={accessTicketDetails}
+              price="10.000"
+            />
+          </div>
+          <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
+            <TicketType
+              image={premiumImage}
+              category="Premium"
+              details={premiumTicketDetails}
+              price="30.000"
+            />
+          </div>
+          <div className="col-12  sm:col-6 lg:col-12 xl:col-4 p-2">
+            <TicketType
+              image={vipImage}
+              category="VIP"
+              details={vipTicketDetails}
+              price="60.000"
+            />
+          </div>
         </div>
       </div>
       <PageFooter />
